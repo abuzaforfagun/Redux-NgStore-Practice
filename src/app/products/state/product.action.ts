1a425b23f1781a1cf77405052104b293b59c123b
Fagun <mdabuzaforfagun@gmail.com>
2018-12-05 01:43:09 +0600

    Add actions for product load

--- src/app/products/state/product.action.ts
+++ src/app/products/state/product.action.ts
@@ -5,7 +5,10 @@ export enum ProductActionTypes {
     ToggleProductCode = '[Product] Toogle Product Code',
     SetCurrentProduct = '[Product] Set Current Product',
     ClearCurrentProduct = '[Product] Clear Current Product',
-    InitializeNewProduct = '[Product] Initialize New Product'
+    InitializeNewProduct = '[Product] Initialize New Product',
+    Load = '[Product] Load',
+    LoadSuccess = '[Product] Loaded Successfully',
+    LoadFail = '[Product] Load Fail'
 }
 
 export class ToogleProductCode implements Action {
@@ -27,4 +30,17 @@ export class InitializeNewProduct implements Action {
     constructor() { }
 }
 
+export class Load implements Action {
+    readonly type = ProductActionTypes.Load;
+}
+
+export class LoadSuccess implements Action {
+    readonly type = ProductActionTypes.LoadSuccess;
+    constructor(public payload: Product[]) { }
+}
+
+export class LoadFail implements Action {
+    readonly type = ProductActionTypes.LoadFail;
+    constructor(public payload: String) { }
+}
 export type ProductActions = ToogleProductCode | SetCurrentProduct | ClearCurrentProduct | InitializeNewProduct;
